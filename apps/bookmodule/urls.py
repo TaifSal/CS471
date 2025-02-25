8522bb093b4ca16fc4b4f4280cd03a8bad10b4c7
TaifSal <137648908+TaifSal@users.noreply.github.com>
2025-02-25 22:34:33 +0300

    lab_4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .import views
+
+
 urlpatterns = [
+    #lab 3
     path('', views.index),
     path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>', views.viewbook)
+    path('<int:bookId>', views.viewbook),
+    #lab 4
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('about_us/', views.about_us, name="books.about_us"),
 ]
+
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-###
-def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html" , {"name": name})
-
+# lab-3
+#def index(request):
+#    name = request.GET.get("name") or "world!"
+#    return render(request, "bookmodule/index.html" , {"name": name})
 def index2(request, val1=0):
-    return render(request, "bookmodule/index2.html", {"value": val1})
+   return render(request, "bookmodule/index2.html", {"value": val1})
 
 def viewbook(request, bookId):
     # assume that we have the following books somewhere (e.g. database)
@@ -18,4 +17,18 @@ def viewbook(request, bookId):
     if book2['id'] == bookId: targetBook = book2
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodule/show.html', context)
+
+### Lab-4
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def about_us(request):
+    return render(request, 'bookmodule/about_us.html')
+
 
